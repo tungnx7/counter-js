73805e542433a24ac45d5f67663d961e2c3c3c50
tungnx7 <ngotung7795@gmail.com>
2022-07-28 16:08:03 +0700

    counter project

---
+++ index.js
@@ -0,0 +1,22 @@
+let number = 0;
+
+const numberEl = document.querySelector(".number");
+const btnDecrease = document.querySelector("#decrease");
+const btnReset = document.querySelector("#reset");
+const btnIncrease = document.querySelector("#increase");
+
+
+
+btnDecrease.addEventListener("click", () => {
+  number -= 1;
+  numberEl.textContent = number;
+});
+
+btnReset.addEventListener("click", () => {
+  numberEl.textContent = 0;
+});
+
+btnIncrease.addEventListener("click", () => {
+  number += 1;
+  numberEl.textContent = number;
+});
